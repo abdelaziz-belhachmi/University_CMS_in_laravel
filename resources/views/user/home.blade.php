fb360dfc6fc0ada1d2dae9bba57e3b2cc1df7895
aiaklf02 <aya.khalifi@etu.uae.ac.ma>
2024-01-25 17:53:40 +0100

    espaceEtud

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,11 +1,50 @@
 @extends('layout.layout') <!-- Assuming you have a master layout, modify this as needed -->
-@section('title',' espace etudiant')
+@section('title',' dashboard')
 @section('content')
 
-<div style="min-height: 80vh">
+<link rel="stylesheet" href="../css/auth_home.css">
+   {{-- <span class="bckg"></span> --}}
+   <div style="display: flex">
 
-  <h1>Welcome User, {{ Auth::user()->name}}
-  </h1>
+<header style="background-color:white;height:auto;min-height:80vh;">
+  {{-- <h1 id="acc" style="justify-content: center;align-content:center;display:flex;background-color:white"></h1> --}}
+  <nav style="">
+    <ul>
+      <li>
+        <a  href="{{route('user.profil')}}">Mon profil</a>
+      </li>
+    
+      <li>
+        <a  href="{{route('user.accueil')}}">Accueil</a>
+      </li>
+
+      <li>
+        <a href="{{route('user.demande')}}" data-title="Annonces">Faire une demande</a>
+      </li>
+
+      <li>
+         <a href="{{url('logout')}}" data-title="Logout">Se deconnecter</a>
+      </li>
+    
+    </ul>
+
+
+  </nav>
+</header>
+
+<main style="float: right;background-color:white">
+
+ 
+
+  @yield('frameContent')
+
+</main>
+
+
 </div>
 
+
+
 @endsection
+
+  
---
+++ resources/views/user/profil.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.layout') <!-- Assuming you have a master layout, modify this as needed -->
+@section('title', 'profil')
+@section('content')
+
+<link rel="stylesheet" href="../css/profil.css">
+
+
+<div id="profil">
+    <img src ="/images/icon/userpic.jpg">
+    <h3> Nom et prenom</h3>
+    <p>CNE:</p>
+    <p>CNI:</p>
+
+</div>
+<div id ="infos_etudes">
+<ul>
+    <li id="annee currente">Annee currente:</li>
+    <li id="filiere">La filiere: </li>
+    <li id="codapogee">Code d'apogee:</li>
+</ul>
+
+</div>
+<div id ="infos_pers">
+    <ul>
+        <li id="adresse">Adresse: </li>
+        <li id="bdd">Date de naissance: </li>
+        <li id="email"> Email:</li>
+    </ul>
+ 
+</div>
