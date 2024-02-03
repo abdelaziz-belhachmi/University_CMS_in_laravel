ef7b503641d341952e52fd0714bb70f82c15e5c2
abdelaziz-belhachmi <abdelaziz.belhachmi@etu.uae.ac.ma>
2024-02-03 11:03:54 +0100

    quick fix

--- app/Http/Controllers/CalendrierController.php
+++ app/Http/Controllers/CalendrierController.php
@@ -27,29 +27,6 @@ class CalendrierController extends Controller {
 
     function emploiDutemps(){
 
-// fetching reservation for that perticular week
-        
-        // $today = now();
-        // $nextWeek = $today->copy()->addWeek();
-        
-        // $structuredReservations = [];
-        
-        // foreach (["9", "11", "13", "15", "17"] as $startTime) {
-        //     $reservationsForStartTime = [];
-        
-        //     for ($day = $today->copy(); $day->lte($nextWeek); $day->addDay()) {
-        //         $reservationsForDay = Reservation::where('day', $day->day)
-        //             ->where('month', $day->month)
-        //             ->where('year', $day->year)
-        //             ->where('start_time', $startTime)
-        //             ->get();
-        
-        //         $reservationsForStartTime[$day->format('d/m/Y')] = $reservationsForDay;
-        //     }
-        
-        //     $structuredReservations[$startTime] = $reservationsForStartTime;
-        // }
-        
         $today = now();
         $nextWeek = $today->copy()->addWeek();
         $structuredReservations = [];
@@ -64,29 +41,41 @@ class CalendrierController extends Controller {
                     ->where('start_time', $startTime)
                     ->get();
     
-                // Check user's role and filter reservations accordingly
                 $userRole = Auth::user()->role;
     
                 if ($userRole == 0) {
+
                     $userClassId = Auth::user()->etudiant->classes_id;    
-                    // Filter reservations based on class ID
+                    
                     $reservationsForDay = $reservationsForDay->where('classes_id', $userClassId);
-                } elseif ($userRole == 1 || $userRole == 2) {
-                    // Filter reservations by user_id
+                
+                }elseif ($userRole == 1 ) {
+
+                    $modules = module::where('professeurs_id',Auth::user()->professeur->id)->first();
+
+                    $modulesID = $modules->id;
+
+                    $reservationsForDay = $reservationsForDay->where('modules_id', $modulesID);
+
+                } elseif ($userRole == 2) {
+
                     $reservationsForDay = $reservationsForDay->where('user_id', Auth::user()->id);
+
                 } elseif ($userRole == 3) {
-                    // Assuming you have the user's departement ID in $userDepartementId
+                    
                     $userDepartementId = Auth::user()->Chef_Departement->departement_id;
     
-                    // Filter reservations based on departement ID
                     $reservationsForDay = $reservationsForDay->filter(function ($reservation) use ($userDepartementId) {
                         return $reservation->local->departement_id == $userDepartementId;
                     });
+
                 } elseif ($userRole == 4) {
+
                     // Filter reservations by locals that belong to null departement ID
                     $reservationsForDay = $reservationsForDay->filter(function ($reservation) {
                         return $reservation->local->departement_id == null;
                     });
+
                 }
     
                 $reservationsForStartTime[$day->format('d/m/Y')] = $reservationsForDay;
